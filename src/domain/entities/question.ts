2f21bcdcd19f864f2380ea2e9e536198025d33ac
PauloStraforini <paulostraforini@gmail.com>
2025-09-13 15:32:44 -0300

    feat(orders): implement updates

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,13 +1,25 @@
 import { Slug } from "./value-objects/slug.js"
 import { Entity } from "../../core/entities/entity.js"
+import type { UniqueEntityID } from "../../core/entities/unique-entity-id.js"
 
 interface QuestionProps {
   title: string
+  bestAnswerId?: UniqueEntityID
   content: string
   slug: Slug
   authorId: string
+  createdAt: Date
+  updateAt?: Date
 }
 
 export class Question extends Entity<QuestionProps> {
+  static create(props: QuestionProps, id?: UniqueEntityID) {
+    const question = new Question({
+      ...props,
+      createdAt: new Date(),   
+    }, id)
 
+    return question
+  }
 }
+
