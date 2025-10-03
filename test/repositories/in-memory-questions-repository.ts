6726ea5630951d3fd21452cb1d32edefe361fc7c
PauloStraforini <paulostraforini@gmail.com>
2025-10-02 23:17:32 -0300

    refactor(in-memory-questions-repository.ts): update repository

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -4,6 +4,16 @@ import { Question } from '@/domain/forum/enterprise/entities/question'
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = []
 
+  async findById(id: string) {
+    const question = this.items.find((items) => items.id.toString() === id)
+
+    if (!question) {
+      return null
+    }
+
+    return question
+  }
+
   async findBySlug(slug: string) {
     const question = this.items.find((items) => items.slug.value === slug)
 
@@ -16,5 +26,11 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
 
   async create(question: Question) {
     this.items.push(question)
+  }
+
+  async delete(question: Question): Promise<void> {
+    const itemIndex = this.items.findIndex((items) => items.id === question.id)
+
+    this.items.splice(itemIndex, 1)
   }
 }
