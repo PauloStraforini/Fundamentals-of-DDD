d59dce58b20f2f436096d93e69af54cbee86f53b
PauloStraforini <paulostraforini@gmail.com>
2025-10-10 11:22:22 -0300

    feat(comments): add polymorphic comment entities

---
+++ src/domain/forum/enterprise/entities/question-comment.ts
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/adjacent-overload-signatures */
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Optional } from '@/core/types/optional'
+import { Comment, CommentProps } from './comment'
+
+export interface QuestionProps extends CommentProps {
+  questionId: UniqueEntityID
+}
+
+export class Question extends Comment<QuestionProps> {
+  get questionId() {
+    return this.props.questionId
+  }
+
+  static create(
+    props: Optional<QuestionProps, 'createdAt'>,
+    id?: UniqueEntityID,
+  ) {
+    const question = new Question(
+      {
+        ...props,
+        createdAt: new Date(),
+      },
+      id,
+    )
+
+    return question
+  }
+}
