16d786436685560a6d77eb0702e4a16c53aae37f
PauloStraforini <paulostraforini@gmail.com>
2025-10-17 17:06:04 -0300

    feat(questions): allow deleting attachments from question

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,12 +1,17 @@
 import { PaginationParams } from '@/core/repositories/pagination-params'
+import { QuestionAttachmentsRepository } from '@/domain/forum/application/repositories/question-attachments-repository'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
 import { Question } from '@/domain/forum/enterprise/entities/question'
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = []
 
+  constructor(
+    private questionAttachmentsRepository: QuestionAttachmentsRepository,
+  ) {}
+
   async findById(id: string) {
-    const question = this.items.find((items) => items.id.toString() === id)
+    const question = this.items.find((item) => item.id.toString() === id)
 
     if (!question) {
       return null
@@ -16,7 +21,7 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
   }
 
   async findBySlug(slug: string) {
-    const question = this.items.find((items) => items.slug.value === slug)
+    const question = this.items.find((item) => item.slug.value === slug)
 
     if (!question) {
       return null
@@ -38,14 +43,18 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
   }
 
   async save(question: Question) {
-    const itemIndex = this.items.findIndex((items) => items.id === question.id)
+    const itemIndex = this.items.findIndex((item) => item.id === question.id)
 
     this.items[itemIndex] = question
   }
 
-  async delete(question: Question): Promise<void> {
-    const itemIndex = this.items.findIndex((items) => items.id === question.id)
+  async delete(question: Question) {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id)
 
     this.items.splice(itemIndex, 1)
+
+    this.questionAttachmentsRepository.deleteManyByQuestionId(
+      question.id.toString(),
+    )
   }
 }
